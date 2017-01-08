174f7cb307cb6f0b062461995c3f821f8072c894
Piyush Agarwal <piyush.agarwal2@dcsg.com>
2017-01-08 02:13:30 -0500

    Added more actions like off thermostat , music player on off.

--- src/main/java/com/connectcar/webhook/WebhookRequestProcessor.java
+++ src/main/java/com/connectcar/webhook/WebhookRequestProcessor.java
@@ -9,6 +9,9 @@ package com.connectcar.webhook;
 public class WebhookRequestProcessor {
 
     private static String THERMO_ON = "thermostate-on";
+    private static String THERMO_OFF = "thermostate-off";
+    private static String MUSIC_ON = "music-on";
+    private static String MUSIC_OFF = "music-off";
 
     public static ResponseActions.ActionOnGoogle processRequest(WebhookRequest request) {
 
@@ -17,11 +20,19 @@ public class WebhookRequestProcessor {
         if (action.equalsIgnoreCase(THERMO_ON)) {
 
             return ResponseActions.ActionOnGoogle.THERMOSTAT_ON;
+        } else if (action.equalsIgnoreCase(THERMO_OFF)) {
+
+            return ResponseActions.ActionOnGoogle.THERMOSTAT_OFF;
+        } else if (action.equalsIgnoreCase(MUSIC_ON)) {
+
+            return ResponseActions.ActionOnGoogle.START_MUSIC;
+        } else if (action.equalsIgnoreCase(MUSIC_OFF)) {
+
+            return ResponseActions.ActionOnGoogle.STOP_MUSIC;
         }
 
 
         return ResponseActions.ActionOnGoogle.SORRY;
-
 
     }
 }
