70a4a74baa5d5e16213d84fc81f8a26d3e78bf42
Piyush Agarwal <piyush.agarwal2@dcsg.com>
2017-01-08 06:14:55 -0500

    Change webhook response

--- src/main/java/com/connectcar/webhook/ResponseActions.java
+++ src/main/java/com/connectcar/webhook/ResponseActions.java
@@ -36,6 +36,12 @@ public class ResponseActions {
 
         Google google = new Google();
 
+        PermissionsRequest permissionsRequest = new PermissionsRequest();
+
+        permissionsRequest.setOptContext("");
+
+        permissionsRequest.setPermissions(new ArrayList<String>());
+
         google.setExpectUserResponse(true);
         google.setIsSsml(true);
         google.setPermissionsRequest(new PermissionsRequest());
