822ecdddaf307063e6bd850dbe6fa7a8a16d1d98
Piyush Agarwal <piyush.agarwal2@dcsg.com>
2017-01-10 23:16:07 -0500

    Corrected speech text.

--- src/main/java/com/connectcar/processor/Speeches.java
+++ src/main/java/com/connectcar/processor/Speeches.java
@@ -7,8 +7,8 @@ public class Speeches {
 
     public static String THERMOSTAT_ON_SPEECH = "Ok <user> I am turning on your <device> in car.";
     public static String THERMOSTAT_OFF_SPEECH = "Ok <user> I am turning off your <device> in car";
-    public static String MUSIC_OFF_SPEECH = "Ok <user> I am turning on your <device>. Enjoy";
-    public static String MUSIC_ON_SPEECH = "Ok <user> I am turning off your music player. Enjoy";
+    public static String MUSIC_OFF_SPEECH = "Ok <user> I am turning off your <device>. Enjoy";
+    public static String MUSIC_ON_SPEECH = "Ok <user> I am turning on your music player. Enjoy";
     public static String SORRY_COULD_NOT_UNDERSTAND = "Sorry <user>, I could not understand what you want to say. Try again.";
 
 
